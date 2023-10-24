cc66cac117f6c3786dde60053e09e08f80c0f2be
MichalJedinak <109184193+MichalJedinak@users.noreply.github.com>
2023-10-24 18:56:08 +0200

    warehouseClient

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.example.demo.gui.rest.WarehouseClient;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		WarehouseClient restClient = new WarehouseClient();       
 		
+		String response = restClient.getDataFromServer();
 		SpringApplication.run(DemoApplication.class,args); 
 	
 	}
