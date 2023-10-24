6e3cb1cb5bd646ab7455059df75e5328fc339dc1
MichalJedinak <109184193+MichalJedinak@users.noreply.github.com>
2023-10-24 17:19:15 +0200

    change field in datahandling api

--- src/main/java/com/example/demo/gui/rest/WarehouseRest.java
+++ src/main/java/com/example/demo/gui/rest/WarehouseRest.java
@@ -52,8 +52,10 @@ public class WarehouseRest  extends JFrame implements ActionListener{
       JTextField field_1 = new JTextField(15);
       JLabel label_2 = new JLabel("Name ");
       JTextField field_2 = new JTextField(15);
-      JLabel descrLabel = new JLabel("Description");
-      JTextArea descArea = new JTextArea();
+      JLabel label_6 = new JLabel("Desription ");
+      JTextField field_6 = new JTextField(15);
+       JLabel label_7 = new JLabel("Desription ");
+      JTextField field_7 = new JTextField(15);
       JLabel label_3 = new JLabel("Available ");
       JTextField field_3 = new JTextField(15);
       JButton update= new JButton("Edit item");
@@ -80,7 +82,6 @@ public class WarehouseRest  extends JFrame implements ActionListener{
       
       public WarehouseRest(){
 
-            descArea.setPreferredSize(new Dimension(120, 190));
             pane.setPreferredSize(new Dimension(400, 250));
             pane.setLayout(new ScrollPaneLayout());
             image= icon.getImage();
@@ -109,8 +110,8 @@ public class WarehouseRest  extends JFrame implements ActionListener{
             right.add(field_2,"gap left 25, wrap");
             right.add(label_3,"span 2");
             right.add(field_3,"gap left 25 ,wrap");
-            right.add(descrLabel,"span 2");
-            right.add(descArea,"gap left 25 , wrap");
+            right.add(label_6,"span 2");
+            right.add(field_6,"gap left 25 , wrap");
             right.add(delete,"span 2,gap top 50");
             right.add(update,"gap left 100 ,gap top 50,wrap");
             right.add(back,"center, gap top 40");
@@ -122,6 +123,8 @@ public class WarehouseRest  extends JFrame implements ActionListener{
             right_2.add(field_4,"align left,wrap");
             right_2.add(label_5);
             right_2.add(field_5,"wrap,span 2");
+            right_2.add(label_7);
+            right_2.add(field_7," wrap");
             right_2.add(create,"center, gap top 25");
             
             bar.add(menu);
@@ -142,8 +145,9 @@ public class WarehouseRest  extends JFrame implements ActionListener{
             ////  pridanie do array listov 
             labelList.add(label);labelList.add(info);labelList.add(label_1);
             labelList.add(label_2);labelList.add(label_3);labelList.add(label_4);labelList.add(label_5);
+            labelList.add(label_6);labelList.add(label_7);
             filedList.add(field_1);filedList.add(field_2);filedList.add(field_3);filedList.add(field_4);
-            filedList.add(field_5);
+            filedList.add(field_5);filedList.add(field_6);filedList.add(field_7);
             buttonList.add(button);buttonList.add(update);buttonList.add(delete);buttonList.add(create);
             // jednotné nastavenie pre elementy v array listoch
             for(JLabel label :labelList){
@@ -222,7 +226,8 @@ public class WarehouseRest  extends JFrame implements ActionListener{
                               item_count=" ";
                         }
                        String descr = resulset.getString(4);
-                        String[] dataLists= {idString,item_name,item_count,descr};// pole Stringov
+                       String createdAt = resulset.getString(5);
+                        String[] dataLists= {idString,item_name,item_count,descr,createdAt};// pole Stringov
                         defaultModel.addRow(dataLists);// pridanie row do default modelu
                         
                   }
@@ -271,7 +276,7 @@ public class WarehouseRest  extends JFrame implements ActionListener{
                                     field_1.setText(table.getModel().getValueAt(listItems,0).toString());
                                     field_2.setText(table.getModel().getValueAt(listItems,1).toString());
                                     field_3.setText(table.getModel().getValueAt(listItems,2).toString());
-                                    descArea.setText(table.getModel().getValueAt(listItems, 3).toString());
+                                    field_6.setText(table.getModel().getValueAt(listItems, 3).toString());
                                     this.repaint();                                  
                               }
                               private void repaint() {
@@ -285,7 +290,6 @@ public class WarehouseRest  extends JFrame implements ActionListener{
                   for(JTextField field :filedList){
                         field.setText("");                    
                   }     
-                  descArea.setText("");           
                   showDataFromDatabaseTable();
                   this.repaint();this.validate();
             }
@@ -318,19 +322,18 @@ public class WarehouseRest  extends JFrame implements ActionListener{
       }
 
       public void dataHandlingGuiApiAddItem(){
-                  Item newItem = new Item();
-             try {
-                  String s = field_3.getText();
+            try {
+                  String s = field_5.getText();
                   int available =Integer.parseInt(s) ;
-                  newItem.setName(field_2.getText());
-                  newItem.setAvailable(available);
-                  newItem.setDescription("Zatiaľ  iban takto pokial upravím gui na aktualnu podobu tabulky");
-                     System.out.println(newItem.getName()+" "+newItem.getAvailable());  
-                 // itemRepository.add(newItem);
-                  Integer generatedKey =itemRepository.add( newItem);
-                      if(generatedKey!=null){
-                         System.out.println(generatedKey);
-                      }else{ System.err.println(generatedKey +" neexistuje");}
+                  String name = field_4.getText();
+                  String description = field_7.getText();
+                  Item newItem = new Item();newItem.setName(name);newItem.setAvailable(available);newItem.setDescription(description);
+                  System.out.println(newItem.getName()+" "+newItem.getAvailable()+" "+newItem.getDescription()+" "+newItem.getCreatedAt());  
+                   itemRepository.add(newItem);
+                  // Integer generatedKey =itemRepository.add( newItem);
+                  //     if(generatedKey!=null){
+                  //        System.out.println(generatedKey);
+                  //     }else{ System.err.println(generatedKey +" neexistuje");}
                         
                   } catch (Exception num) {
                         JOptionPane.showMessageDialog(null,num, "Upozornenie",JOptionPane.ERROR_MESSAGE);
@@ -343,11 +346,12 @@ public class WarehouseRest  extends JFrame implements ActionListener{
                   Integer id = Integer.parseInt(i);
                   String name = field_2.getText();
                   int available = Integer.parseInt(field_3.getText());
-                  String description = descArea.getText();
+                  String description = field_6.getText();
                   Item item = new Item();
                   item.setId(id); item.setName(name);item.setAvailable(available);item.setDescription(description);
+                  
                   System.out.println(item.getId()+" : "+item.getName()+" "+item.getAvailable());
-                  //request = new UpdateItemRequest(name, available, description);  
+                  request = new UpdateItemRequest(item.getName(),item.getAvailable(), item.getDescription());  
                   itemRepository.update( id,request);    
                   JOptionPane.showMessageDialog(null, "Pridali ste nový Item :)");
                   System.out.println("Vytvorenie Item prebehlo v poriadku.:)");
